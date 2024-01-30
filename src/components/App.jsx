342abdd6f746e2dfb9b7f7b823d5f44457ad0975
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2024-01-31 11:42:28 +1300

    event handling complex state - form  submittion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState(" ");
+  const [lastName, setLastName] = useState(" ");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function handleFNOnChange(event) {
+    setFirstName(event.target.value);
+ 
+  }
+    function handleLNOnChange(event) {
+
+      setLastName(event.target.value);
+    }
+
+  function handleOnClick(event) {
+    setIsSubmitted(true);
+
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {isSubmitted? firstName + " "+ lastName: null } </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input
+          onChange={handleFNOnChange}
+          value={firstName}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleLNOnChange}
+          value={lastName}
+          name="lName"
+          placeholder="Last Name"
+        />
+        <button
+          style={{ backgroundColor: isSubmitted ? "black" : "white" }}
+          onClick={handleOnClick}
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
 }
 
 export default App;
-
